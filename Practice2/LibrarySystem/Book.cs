bf0010fa5224dda91a3aad8b6e61c8d5098db006
Joel Reyes <jreyesguerrero@deloitte.com>
2024-08-22 17:51:12 -0600

    Added library functionality
    Created the Book class to represent a book entity.
    Created the Library class to manage the books repository.

---
+++ Practice2/LibrarySystem/Book.cs
@@ -0,0 +1,23 @@
+﻿namespace Practice2.LibrarySystem
+{
+    public class Book
+    {
+        public string ISBN { get; init; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public Book(string isbn, string title, string author)
+        {
+            ISBN = isbn;
+            Title = title;
+            Author = author;
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"--- Book {ISBN} ---");
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Author: {Author}");
+        }
+    }
+}
---
+++ Practice2/LibrarySystem/Library.cs
@@ -0,0 +1,27 @@
+﻿namespace Practice2.LibrarySystem
+{
+    public class Library
+    {
+        private readonly List<Book> _books;
+
+        public Library()
+        {
+            _books = new List<Book>();
+        }
+
+        public void AddBook(Book book)
+        {
+            _books.Add(book);
+        }
+
+        public void RemoveBook(Book book)
+        {
+            _books.Remove(book);
+        }
+
+        public void DisplayBooks()
+        {
+            _books.ForEach(book => book.DisplayDetails());
+        }
+    }
+}
