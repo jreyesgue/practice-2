488d7b51df0ffdf8f3517702b0a794f095b7dc65
Joel Reyes <jreyesguerrero@deloitte.com>
2024-08-22 17:31:50 -0600

    Add project files.

---
+++ Practice2/School/Classroom.cs
@@ -0,0 +1,27 @@
+﻿namespace Practice2.School
+{
+    public class Classroom
+    {
+        private readonly List<Student> _students;
+
+        public Classroom()
+        {
+            _students = new List<Student>();
+        }
+
+        public void AddStudent(Student student)
+        {
+            _students.Add(student);
+        }
+
+        public void RemoveStudent(Student student)
+        {
+            _students.Remove(student);
+        }
+
+        public void DisplayStudents()
+        {
+            _students.ForEach(student => student.DisplayDetails());
+        }
+    }
+}
---
+++ Practice2/School/Student.cs
@@ -0,0 +1,27 @@
+﻿namespace Practice2.School
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int RollNumber { get; set; }
+        public double Grade { get; private set; }
+
+        public Student(string name, int rollNumber)
+        {
+            Name = name;
+            RollNumber = rollNumber;
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"--- Student {RollNumber} ---");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Grade: {Grade}");
+        }
+
+        public void CalculateGrade(params int[] marks)
+        {
+            Grade = marks.Average();
+        }
+    }
+}
